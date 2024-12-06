2a1a92d078b7dd8a75dd8e8e16d8ad6f6086745b
AdarshRai3 <adarshaibusiness@gmail.com>
2024-12-06 15:31:51 +0530

    Sql relationships

--- PostgresSql/Advancesql.sql
+++ PostgresSql/Advancesql.sql
@@ -10,3 +10,33 @@ Select avg(salary),dept from employees group by dept;
 --Combine column 
 Select count(fname),avg(salary),dept from employees group by dept;
 
+---------------------------------------------
+Alter command (it is used to change the structure of the table)
+
+Alter table employees remane to emp;
+
+Alter table emp  rename column emp_id to id;
+
+Alter table emp add column city varchar(50) default 'Noida';
+
+Alter table emp Drop column city;
+
+Alter table emp Alter column city set Data type varchar(50);
+
+Alter table emp Alter column  city set default 'Noida';
+-------------------------------------------------------------
+
+-- Check contraints ( use for validation of data by adding constraints)
+
+Alter table emp add constraint mob_no_not_valid check(length(mob)>10);
+
+-----------------------------------------------------------------
+
+---Else is used to get a column value based on conditions; 
+Select salary , else 
+when salary>=10000 then 'High' 
+else 'Low'
+end as salary_stats from employees
+group by salary;
+
+------------------------------------------------------------------
