3935ae9cd679d72757bd6e2f0387dacb9bf44310
AdarshRai3 <adarshaibusiness@gmail.com>
2024-12-19 15:27:39 +0530

    Add : LinkedList class

---
+++ JavaCore/Collection/List/LinkedListMethods.java
@@ -0,0 +1,25 @@
+package JavaCore.Collection.List;
+public class LinkedListMethods {
+    
+    public class Node{
+        private int value;
+        private Node next;
+        private Node prev;
+    }
+    public static void main(String[] args) {  
+
+        //Linked list is better in  performance compare to arraylist in the following context:
+
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Ad");
+        list.add("Raj");
+        list.add("Rajiv"); 
+        list.add("Rama");
+        list.add("Ad");
+        list.remove("Rajiv");
+        LinkedList<String> ls = new ArrayList<>(Array.asList("Karan","Aditya"));
+        list.add(ls);
+        System.out.println(list);
+
+    }  
+}
