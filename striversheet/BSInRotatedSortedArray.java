78fe7f144bfe3bf7bcca2b6a747b62b161b5ef8f
AdarshRai3 <adarshaibusiness@gmail.com>
2024-12-29 23:35:46 +0530

    Solve : BSFindPeakElement

--- striversheet/BSInRotatedSortedArray.java
+++ striversheet/BSInRotatedSortedArray.java
@@ -1,28 +1,6 @@
 public class BSInRotatedSortedArray {
     
     public boolean search(int[] nums, int target){
-        int n = nums.length;
-        int low = 0;
-        int high =0;
-
-        while(low<=high){
-            if(nums[mid]==target){
-                return true;
-            }
-
-            if(nums[high]==nums[mid] && nums[low]==nums[mid]){
-                low=low+1;
-                high=high-1;
-                continue;
-            }
-            
-            if(nums[low]<nums[mid]){
-              
-                if(nums[])
-
-            }else{
-
-            }
-        }
+    
     }
 }
