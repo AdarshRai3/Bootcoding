bd62d09d18be554c9c55f81e4047064269a4efbc
AdarshRai3 <adarshaibusiness@gmail.com>
2025-01-12 07:16:59 +0530

    Learn : vectors in java

--- JavaCore/Collection/List/VectorListMethods.java
+++ JavaCore/Collection/List/VectorListMethods.java
@@ -1,3 +1,4 @@
+import java.util.Vector;
 import java.utils.*;
 public class VectorListMethods {
     //vector is legacy class in java, unlike linked list and arraylist it is synchronized and thread safe 
@@ -12,13 +13,48 @@ public class VectorListMethods {
     vector.add(5);
     vector.add(6);
     vector.add(7);
-    vector
+    vector.remove(2);
+    vector.add(2,4);
+    vector.set(2,6);
+    vector.get(3);
+    vector.size();
+    vector.capcity(5);
+    vector.capicity(3,5);
+    vector.isEmpty();
+    vector.clear();
+    vector.contains(3);
+    vector.indexOf(3);
+    vector.lastIndexOf(3);
+    vector.trimToSize();
+    vector.ensureCapacity(5);
+    vector.toArray();
+    vector.firstIndexof(3);
+
     //Unlike arrayList and linkedList , vector does have method to get the capicity
+    //Default capicity is 10
+    //Default resizing is 2x 
+    //Vector has internally dynamic array impleamentation
+    //Vector are thread safe and synchronized means they can be used in multithreaded environment but but to this they also have an overhead due to locking and unlocking of the thread     
+    // example for vector thread safety 
 
-
+    Thread t1 = new Thread(()->{
+        for(int i =0;i<1000;i++){
+            vector.add(i);
+        }
+    });
+    Thread t2 = new Thread(()->{    
+        for(int i =0;i<1000;i++){
+            vector.add(i);
+        }   
+    });
+    t1.start();
+    t2.start();
+    try{
+        t1.join();
+        t2.join();
+    }catch(InterruptedException e){
+        e.printStackTrace();
     }
-    
-    
-    
-
+    System.out.println(vector.size());
+  }
 }
