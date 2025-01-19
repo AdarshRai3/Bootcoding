e497a73c0cdf7b3fbd01f71e9e821b47e796034d
AdarshRai3 <adarshaibusiness@gmail.com>
2025-01-19 20:35:41 +0530

    Learn : GC

---
+++ JavaCore/Collection/Map/Phone.java
@@ -0,0 +1,34 @@
+public class GC {
+    
+}
+
+public class Phone{
+    String model;
+    Integer price;
+
+    public Phone(String model ,Integer price  ){
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public Integer getPrice(){
+        return price;
+    }
+
+    public String toString(){
+        return "Model :"+model+",  Price :"+price;
+    }
+
+    public setModel(String model){
+        return this.model = model;
+    }
+
+    public setPrice(Integer price){
+        return this.price = price;
+    }
+    
+}
