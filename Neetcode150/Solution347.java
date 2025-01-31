4620e40a1e15e9ad2819dd83cb4b8b8b64a974e1
AdarshRai3 <adarshaibusiness@gmail.com>
2025-01-31 15:23:20 +0530

    Problem 347 workinh on this problem

--- Neetcode150/Solution347.java
+++ Neetcode150/Solution347.java
@@ -1,3 +1,8 @@
 public class Solution347 {
     
 }
+// ---------------------------------------------------------
+// Solution 1 
+Steps of solving this problems : 
+1.To use a hashmap where we will store the elements of the array as key and the frequecy of their occurences inside the array as values.
+2.After than we will iterate over the keys of the hashmap and put them in the 
