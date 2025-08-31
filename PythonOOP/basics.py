6af0ec9292e7e7f95a620e0c3be153e3289c0b58
AdarshRai3 <adarshaibusiness@gmail.com>
2025-08-31 23:56:54 +0530

    Learn:Basic of OOPS

--- PythonOOP/basics.py
+++ PythonOOP/basics.py
@@ -2,16 +2,18 @@ class Car:
     #Constructor
     def __init__ (self , brand:str ,model:str):
         self.brand:str = brand
-        self.brand:str = model
+        self.model:str = model
         
     def introduce(self)->None:
-        print(f"Introducing you car of  {self.brand} and it's name is {self.model}")
+        print(f"Introducing you car of {self.brand} and it's name is {self.model}")
     def drive(self)->None:
-        print(f"{self.brand}{self.model} is driving")
+        print(f"{self.brand} {self.model} is driving")
     
 
 car1 = Car("Tesla","Model X")
 car2 = Car("Toyata","Enevoa")
 
+car1.introduce()
+car2.introduce()
 car1.drive()
 car2.drive()
