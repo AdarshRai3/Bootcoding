33c9a395c6ed32675373cf897a3bfefaf4dec608
AdarshRai3 <adarshaibusiness@gmail.com>
2025-09-18 19:00:58 +0530

    Learn : Borrow-check and Ownership in Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -49,12 +49,20 @@ fn main() {
 
     stack_fn(23,98);
 
-    
+    heap_fn();
 
+    update_string();
 
-    
+    ownership_rule_1();
+    ownership_rule_2();
+    ownership_rule_3();
+    ownership_rule_4();
+    borrowing_rule_1();
+    borrowing_rule_2();
+    borrowing_rule_3();
 
 } 
+
 
 pub fn get_first_word(sentence:String)->String{
     let mut ans:String = String::from("");
@@ -73,10 +81,91 @@ pub fn do_sum(a:i32 , b:i32)->i32{
 
 pub fn stack_fn(a:i32 , b:i32){
   let c = a+b;
-  println!("Sum of {} and {} is {}", a, b,c);
+  println!("Stack function : Sum of {} and {} is {}", a, b,c);
 }
 
-pub fn heap_fn(s1:String , s2:String){
+pub fn heap_fn(){
+
+    let s1:String=String::from("Hello");
+    let s2:String=String::from("Adarsh");
     let combined = format!("{} {}",s1 ,s2);
-    println!("The combined string is {} and {} is {}", s1, s2 , combined);
+    println!("Heap Function : The combined string is {} and {} is {}", s1, s2 , combined);
+}
+
+pub fn update_string(){
+    let mut s:String = String::from("Hello");
+    println!("Before we are updatig the string: {} ",s);
+    println!("Capicity:{} , Length:{} , Pointer:{:p}", s.capacity(), s.len(), s.as_ptr());
+
+    
+    s.push_str("Add some additional text");
+    println!("After we are updating the string: {}",s);
+    println!("Capicity:{} , Length:{} , Pointer:{:p}", s.capacity(), s.len(), s.as_ptr());
+}
+
+pub fn ownership_rule_1(){
+
+    let s1:String = String::from("Ownership Rule 1");
+    println!("s1:{}", s1);
+    println!("As soon as we start giving ownership of the variable to another variable , ownership change that variable which dont have ownershp dies");
+    let s2 = s1;
+    println!("s2:{}", s2);
+}
+
+pub fn ownership_rule_2(){
+    let s1:String = String::from("Owenership Rule 2");
+    println!("s1:{}",s1);
+    let s2:String = s1.clone();
+    println!("s1:{} : Now since we using cloned copy, it will not throw an error",s1);
+    println!("Clone just create another copy of the variable of our variable , so variale is not out of ownership hence we dont get any error");
+    println!("s2:{} :",s2);
+}
+
+pub fn ownership_rule_3(){
+    let my_string:String = String::from("Owenership_Rule_3");
+    take_ownership_rule_1(my_string);
+    println!("when the string is passed in other function, it changed its ownership");
+    println!("now if we call my string varible after passing it in a function it will give us an error")
+}
+
+pub fn take_ownership_rule_1(some_string:String){
+    println!("Some string input is print here:{}", some_string)
+}
+pub fn ownership_rule_4(){
+    let mut my_string=String::from("Ownership_Rule_4");
+    my_string = take_ownership_rule_2(my_string);
+    println!("String ownership : {}" , my_string);
+    println!("Even through we are passing a string in the function, but since the string is mutable , hence it value can be changed , since we dont violated previous ownership rules and it still works, this is like we are giving ownership to someone else then taking back the ownership ")
+}
+
+pub fn take_ownership_rule_2(some_string:String)->String{
+    println!("my_string {}", some_string);
+    return some_string;
+}
+
+pub fn borrowing_rule_1(){
+    let s1: String = String::from("Borrowing_Rule_1");
+    println!("s1:{}", s1);
+    let s2: &String = &s1;
+    println!("s1:{} : Here s1 will still print without throwing an error since we are passing it value as reference means s2->s1-> value",s1);
+    println!("s2:{}",s2);
+}
+
+pub fn borrowing_rule_2(){
+    let my_string:String = String::from("Borrowing_Rule_2");
+    println!("s1:{}",my_string);
+    borrow_variable(&my_string);
+}
+
+pub fn borrow_variable(some_string:&String){
+    println!("some string {}", some_string);
+}
+
+
+pub fn borrowing_rule_3(){
+    let mut s1: String = String::from("Borrowing_Rule_3");
+    let s2 : &mut String = &mut s1;
+    s2.push_str(": Rule successfully understood");
+    println!("{}",s1);
+    println!("If our variable has one mutable reference, then the caraible cannot have anymore mutable and immutable references")
 }
